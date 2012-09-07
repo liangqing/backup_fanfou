8b6ea758fbcfab64e43becd14a13b6da552d3d86
liangqing <liangqing226@gmail.com>
2012-09-07 21:39:19 +0800

    only self

--- backup.py
+++ backup.py
@@ -21,6 +21,7 @@ def create_tables(conn):
     repost_status_id text,
     repost_user_id text,
     repost_screen_name text,
+    photo text,
     user text
     )''')
 
@@ -66,31 +67,7 @@ def save(conn, sql, keys, data):
       values.append('')
     markers.append('?')
   sql = sql % (','.join(keys), ','.join(markers))
-  try:
-    conn.execute(sql, tuple(values))
-  except sqlite3.IntegrityError,x:
-    print x, data['id']
-    pass
-  conn.commit()
-
-def save_message(conn, message):
-  save(conn, '''insert into message (%s) values(%s)''',
-    '''id,
-    created_at,
-    rawid,
-    text,
-    source,
-    truncated,
-    in_reply_to_status_id,
-    in_reply_to_user_id,
-    favorited,
-    in_reply_to_screen_name,
-    is_self,
-    location,
-    repost_status_id,
-    repost_user_id,
-    repost_screen_name,
-    user''', message)
+  conn.execute(sql, tuple(values))
 
 def save_user(conn, user):
   save(conn, '''insert into user (%s) values(%s)''', 
@@ -121,14 +98,55 @@ def save_user(conn, user):
     profile_background_image_url,
     profile_background_tile''', user)
 
-def fetch(opener, userid=None, max_id=None):
-  url = 'http://api.fanfou.com/statuses/'
+
+def save_message(conn, message):
+
+  #save user
+  user = message['user'].copy()
+  if user.has_key('created_at'):
+    user['created_at'] = time(user['created_at'])
+  try:
+    save_user(conn, user)
+  except sqlite3.IntegrityError,x:
+    pass
+
+  #convert message data
+  message['user'] = user['id']
+  if message.has_key('created_at'):
+    message['created_at'] = time(message['created_at'])
+
+  if message.has_key('photo'):
+    message['photo'] = json.dumps(message['photo'])
+
+  try:
+    save(conn, '''insert into message (%s) values(%s)''',
+      '''id,
+      created_at,
+      rawid,
+      text,
+      source,
+      truncated,
+      in_reply_to_status_id,
+      in_reply_to_user_id,
+      favorited,
+      in_reply_to_screen_name,
+      is_self,
+      location,
+      repost_status_id,
+      repost_user_id,
+      repost_screen_name,
+      photo,
+      user''', message)
+  except sqlite3.IntegrityError, x:
+    print x, message['id']
+  conn.commit()
+
+  #if message.has_key('repost_status'):
+    #save_message(conn, message['repost_status'])
+
+def fetch(opener, max_id=None):
+  url = 'http://api.fanfou.com/statuses/user_timeline.json'
   data = {'count':60}
-  if userid is not None:
-    url += 'user_timeline.json'
-    data['id'] = userid
-  else:
-    url += 'home_timeline.json'
   if max_id is not None:
     data['max_id'] = max_id
   params = urllib.urlencode(data)
@@ -147,21 +165,11 @@ if __name__ == '__main__':
 
   opener = urllib.FancyURLopener()
 
-  userid = raw_input("please input the user you want backup\n")
-  if not userid or userid == '':
-      userid = None
   max_id = None
   
   while True:
-    messages = fetch(opener, userid, max_id)
+    messages = fetch(opener, max_id)
     for message in messages:
-      user = message['user'].copy()
-      if message.has_key('created_at'):
-        message['created_at'] = time(message['created_at'])
-      if user.has_key('created_at'):
-        user['created_at'] = time(user['created_at'])
-      save_user(conn, user)
-      message['user'] = user['id']
       save_message(conn, message)
       max_id = message['id']
     if len(messages) < 60:
